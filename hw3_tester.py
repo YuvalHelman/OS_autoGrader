65c8b58d1da94cc71ec13cae71736e8a70f1c771
yuvalHelman <yuvalasaf3@gmail.com>
2019-05-24 17:25:44 +0300

    first push with ex1 Files ready

--- hw3_tester.py
+++ hw3_tester.py
@@ -1,81 +1,205 @@
 import os
 import subprocess as sp
+import sys
+
 import utils
 from utils import build_comments
 from utils import write_to_csv
 
 
 def compile_files(exe_files_path, compile_log):
-    p = sp.Popen(args=['gcc', '-o3', '-Wall', '-std=c99', "hw1_concat.c"
+    # p = sp.Popen(args=['pwd'], #debug
+    #              cwd=exe_files_path,
+    #              stdout=compile_log, stderr=compile_log
+    #              )
+    p = sp.Popen(args=['gcc', '-o3', '-Wall', '-std=gnu99', "message_reader"
         , "-o", "message_reader.c"],
                  cwd=exe_files_path,
                  stdout=compile_log, stderr=compile_log
                  )
     p.wait()
-    p = sp.Popen(args=['gcc', '-o3', '-Wall', '-std=c99', "hw1_concat.c"
-        , "-o", "message_sender.c"],
+    if p.returncode != 0:  # check if compilation works
+        return 1
+    p = sp.Popen(args=['gcc', '-o3', '-Wall', '-std=gnu99', "message_sender.c"
+        , "-o", "message_sender"],
                  cwd=exe_files_path,
                  stdout=compile_log, stderr=compile_log
                  )
     p.wait()
-
     if p.returncode != 0:  # check if compilation works
         return 1
-    else:  # tests
-        return 0
+    p = sp.Popen(args=['make'],
+                 cwd=exe_files_path,
+                 stdout=compile_log, stderr=compile_log
+                 )
+    p.wait()
+    if p.returncode != 0:  # check if compilation works
+        return 1
+
+    # Check if the .ko file was created
+    if (os.path.exists(exe_files_path + "message_slot.ko") == 1):
+        return 1
+
+    return 0
+
 
 def iterate_students_directories():
     utils.open_names_csv()
 
     directory_str = "./assignments/"
-    compile_log = open('compilation_log.txt', 'w')
-    compile_log.write('.\n')
-    compile_log.close()
-    compile_log = open('compilation_log.txt', 'a')
+    with open('compilation_log.txt', 'w') as compile_log:
+        compile_log.write('.\n')
+
+    with open('compilation_log.txt', 'a') as compile_log:
+        for student_dir in os.listdir(directory_str):  # iterate on all student folders!
+            splitted_filename = student_dir.split("_")
+            student_id = splitted_filename[1]
+            student_name = splitted_filename[0]
+            student_GRADE = 100
+            student_comment = ''
+
+            exe_files_path = directory_str + student_dir + "/"  # ->  ./assignments/Yuval Checker_999999999/
+            log_name_path = exe_files_path + 'opLog.txt'
+            with open(log_name_path, 'w') as val_log:  # a file to throw logs for debugging
+                val_log.write('.\n')
+            try:
+                compile_log.write(
+                    "Starting on: {}\n".format(student_dir))
+
+                if (compile_files(exe_files_path, log_name_path) != 0):
+                    print("{}".format(student_name), " Compilation Failed")
+                    write_to_csv(student_name, student_id, 0, 'Compilation error')
+                else:  # tests
+                    # print("student {} ".format(student_name), "compilation successful")
+                    points_to_reduct, is_mem_leak, is_test_errors = run_tests(exe_files_path)
+                    student_GRADE -= points_to_reduct
+                    student_comment = build_comments(is_mem_leak, is_test_errors)
+                    write_to_csv(student_name, student_id, student_GRADE, student_comment)
+
+            except OSError as e:
+                print("OSError1: ", e)
+            except ValueError as e2:
+                print("ValueError1: ", e2)
 
 
-    for student_dir in os.listdir(directory_str):  # iterate on all student folders!
-        splitted_filename = student_dir.split("_")
-        student_id = splitted_filename[1]
-        student_name = splitted_filename[0]
-        student_GRADE = 100
-        student_comment = ''
+def send_message(file_path_to_exe, log_name_path, dev_name, write_mode, chID, msgStr):
+    device_path = "/dev/%s".format(dev_name)
+    try:
+        # Using the user's "Message Sender"
+        p = sp.Popen(args=['./message_sender', device_path, write_mode, chID, msgStr],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+        if (p.returncode != 0):
+            print("message_sender failed", file_path_to_exe)
+            return 1
+        # change the created file's permissions
+        p = sp.Popen(args=['chmod 777 /dev/%s'.format(dev_name)],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
 
-        exe_files_path = directory_str + student_dir + "/"  # ->  ./assignments/Yuval Checker_999999999/
-        try:
-            compile_files(exe_files_path, compile_log)
-            compile_log.write("Starting on: {}\n".format(student_dir))  # Not syncronized.. but doesnt matter At the moment
-            # p = sp.Popen(args=['pwd'], #debug
-            #              cwd=exe_files_path,
-            #              stdout=compile_log, stderr=compile_log
-            #              )
-            p = sp.Popen(args=['gcc', '-o3', '-Wall', '-std=c99', "hw1_concat.c"
-                , "-o", "hw1_concat"],
-                         cwd=exe_files_path,
-                         stdout=compile_log, stderr=compile_log
-                         )
-            p.wait()
-            if p.returncode != 0:  # check if compilation works
-                student_GRADE = 0
-                student_comment = 'Compilation error'
-                print("{}".format(student_name), " Compilation Failed")
-                write_to_csv(student_name, student_id, student_GRADE, student_comment)
-            else:  # tests
-                # print("student {} ".format(student_name), "compilation successful")
-                points_to_reduct, is_mem_leak, is_test_errors = run_tests(exe_files_path)
-                student_GRADE -= points_to_reduct
+    except ValueError as e:
+        print("parsed major number error. %s", e)
+        return 1
+    except OSError as e:
+        print("OSError22: ", e)
+        return 1
 
-                student_comment = build_comments(is_mem_leak, is_test_errors)
-                write_to_csv(student_name, student_id, student_GRADE, student_comment)
-
-        except OSError as e:
-            print("OSError1: ", e)
-        except ValueError as e2:
-            print("ValueError1: ", e2)
-
-    compile_log.close()
+    return 0
 
 
+def create_char_device(file_path_to_exe, log_name_path, majorNumber, minorNumber, dev_name):
+    try:
+        # Use mknod
+        p = sp.Popen(args=['mknod /dev/%s'.format(dev_name), 'c', majorNumber, minorNumber],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+        if (p.returncode != 0):
+            print("mknod failed", file_path_to_exe)
+            return 1
+        # change the created file's permissions
+        p = sp.Popen(args=['chmod 777 /dev/%s'.format(dev_name)],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+
+    except ValueError as e:
+        print("parsed major number error. %s", e)
+        return 1
+    except OSError as e:
+        print("OSError22: ", e)
+        return 1
+
+    return 0
+
+
+def load_module(file_path_to_exe, log_name_path):
+    dmesg_file = file_path_to_exe + 'dmesg_file.txt'
+    MajorNum_file = file_path_to_exe + 'dmesg_file.txt'
+    majorNumber = 0
+    try:
+        p = sp.Popen(args=['sudo insmod message_slot.ko'],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+        if (p.returncode == 1):
+            print("insmod failed for user: %s", file_path_to_exe)
+            return 1, -1
+
+        #  load the last message of "dmesg" into a MajorNum_file.
+        p = sp.Popen(args=['dmesg >', dmesg_file],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+        p = sp.Popen(args=['tail -1', dmesg_file, '>', MajorNum_file],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+        # read the file into a string and fetch the MajorNumber
+        with open(MajorNum_file, 'r') as o_log:
+            majorNumber = int(o_log.readlines()[0].split(' ')[7])
+
+    except ValueError as e:
+        print("parsed major number error. %s", e)
+        return 1, -1
+    except OSError as e:
+        print("OSError22: ", e)
+        return 1, -1
+
+    return 0, majorNumber
+
+
+def remove_module(file_path_to_exe, log_name_path):
+    try:
+        p = sp.Popen(args=['sudo rmmod message_slot'],
+                     cwd=file_path_to_exe,
+                     stdout=log_name_path, stderr=log_name_path
+                     )
+        p.wait()
+        if (p.returncode == 1):
+            print("rmmod failed for user: %s", file_path_to_exe)
+            sys.exit()  # DEBUG: if I cant remove the Module, I shouldn't run the other directories until its off
+    except ValueError as e:
+        print("parsed major number error. %s", e)
+        return 1
+    except OSError as e:
+        print("OSError22: ", e)
+        return 1
+
+    return 0
+
+
+points_to_reduct_for_test = 3  # TODO: change this to whatever would work with the tests
+points_to_reduct_mem_leak = 1  # TODO: change this to whatever would work with the tests
 ''' 
 Checks the tests on 1 student
 Returns the number of points needed to reduct from the student
@@ -84,22 +208,24 @@ Returns the number of points needed to reduct from the student
 
 def run_tests(file_path_to_exe):  # ./assignments/Yuval Checker_999999999/
     # TODO: go over all of the directories I used.. didn't put too much effort into doing this in this function
-    log_name_path = file_path_to_exe + 'valgrind_log.txt'
-    val_log = open(log_name_path, 'w')  # a File to throw logs for debugging
-    val_log.write('.\n')
-    val_log.close()
+    log_name_path = file_path_to_exe + 'opLog.txt'
+    with open(log_name_path, 'w') as val_log:  # a File to throw logs for debugging
+        val_log.write('.\n')
     val_log = open(log_name_path, 'a')
 
     supp_file_path = "../../supp_file.supp"
     valgrind_err_num = 33
     points_to_reduct = 0
-    points_to_reduct_for_test = 3  # TODO: change this to whatever would work with the tests
-    points_to_reduct_mem_leak = 1  # TODO: change this to whatever would work with the tests
     is_mem_leak, is_test_errors = False, False
 
     # os.putenv(varname, value)
     os.unsetenv("HW1TF")
     os.unsetenv("HW1DIR")
+
+    try:
+        load_module(file_path_to_exe, log_name_path)
+    except OSError as e:
+        print("OSError22: ", e)
 
     try:  # check "not defined ENV variables"
         p = sp.Popen(args=["./hw1_concat", "a", "b"],
