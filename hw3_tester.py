2f603683d0d9999c7cd2b344baf873b3c62c7917
yuvalHelman <yuvalasaf3@gmail.com>
2019-06-02 18:14:43 +0300

    fixing insmod

--- hw3_tester.py
+++ hw3_tester.py
@@ -151,7 +151,8 @@ def load_module(file_path_to_exe, log_fd):
     MajorNum_file = file_path_to_exe + 'dmesg_file.txt'
     majorNumber = 0
     try:
-        p = sp.Popen(args=['bash insmod_turnaround.sh', file_path_to_exe],
+        print(file_path_to_exe)
+        p = sp.Popen(args=['bash insmod_turnaround.sh', './assignments/Yoav_Halperin_313431009/'],
                      stdout=log_fd, stderr=log_fd
                      )
         p.wait()
