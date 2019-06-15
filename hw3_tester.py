c73ac62ba449df9c2b7210f0f8b8f4e832266658
yuvalHelman <yuvalasaf3@gmail.com>
2019-06-15 22:47:13 +0300

    debugging on linux

--- hw3_tester.py
+++ hw3_tester.py
@@ -198,9 +198,10 @@ def load_module(file_path_to_exe, log_fd):
             log_lines_list = dmesg_log.readlines()
             if (log_lines_list):
                 last_line = log_lines_list[len(log_lines_list) - 1]
-                print(last_line)
+
                 studentKernLogMessage = last_line.split(']')[1]
-                print([int(s) for s in str.split(' ') if s.isdigit()][0])  # DEBUG
+                print(studentKernLogMessage)
+                print([int(s) for s in studentKernLogMessage.split(' ') if s.isdigit()][0])  # DEBUG
                 #majorNumber = int(majorNumberStr)
             # p = sp.Popen(args=['cat /var/log/syslog'],
             #              cwd=file_path_to_exe,
